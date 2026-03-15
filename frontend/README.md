d59a59afae70ea7345f48cc4f447e95edf049158
F3NCE-Dev <thefrancandman@gmail.com>
2026-03-15 14:55:13 +0200

    Frontend Init

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# shop-app
+# frontend
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+    <a href="#" class="text-xl font-bold text-slate-900">Shop App</a>
+    <nav>
+      <ul class="flex space-x-6">
+        <li>
+          <a href="#" class="text-slate-700 hover:text-slate-900">Home</a>
+        </li>
+        <li>
+          <a href="#" class="text-slate-700 hover:text-slate-900">About</a>
+        </li>
+        <li>
+          <a href="#" class="text-slate-700 hover:text-slate-900">Contact</a>
+        </li>
+        <li>
+          <a href="#" class="text-slate-700 hover:text-slate-900">Cart</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+>
